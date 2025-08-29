fabee555ea06a79a54a41916e33a61e4035bc8d0
Junajpu <santiagojconos@gmail.com>
2025-08-28 21:36:51 -0600

    Update mongo.js

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -4,7 +4,8 @@ async function connectMongo(uri) {
   mongoose.set("strictQuery", true);
   await mongoose.connect(uri);
   console.log("MongoDB conectado");
-  console.log("Prueba")
+  console.log("Prueba");
+  console.log("Prueba2")
 }
 
 module.exports = { connectMongo };
